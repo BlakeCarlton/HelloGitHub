b4762dacc3639651f7be7cf73162633ed56ea9fa
Blake <BlakeACarlton@gmail.com>
2022-11-11 02:15:51 -0600

    Add initial commit of project.

---
+++ HelloGitHub/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Author: Blake
+ * Date: Nov 11, 2022
+ * File: Main.java
+ * Description: Messing around trying out github
+ */
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub!");
+	}
+
+}
